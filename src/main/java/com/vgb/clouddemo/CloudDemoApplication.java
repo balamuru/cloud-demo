e36f92896c778ba93146ce66fa71fac6f7198024
balamuru <balamuru@gmail.com>
2020-11-23 15:55:39 -0600

    Initial commit

---
+++ src/main/java/com/vgb/clouddemo/CloudDemoApplication.java
@@ -0,0 +1,23 @@
+package com.vgb.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@SpringBootApplication
+public class CloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDemoApplication.class, args);
+	}
+
+
+	@GetMapping("/greet")
+	public String greet() {
+		return "Hello, the time right now is: " + Instant.now();
+	}
+}
